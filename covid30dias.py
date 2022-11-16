171af4a37ebfd118cda2a9ebb29dd3ebde4b3980
Manteiga <rafael.manteiga@hotmail.com>
2022-11-16 03:05:13 -0300

    Add files via upload

--- covid30dias.py
+++ covid30dias.py
@@ -1,7 +1,10 @@
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 dados = []
+mediamovel = []
+cont=0
+soma=0
 
 with open("C:\\Users\\f0fp1107\Desktop\\covid30dias.txt") as f:
     for line in f:
@@ -15,3 +18,11 @@ desv = np.std(dados).round(2)
 #plt.show()
 
 print("A média de casos por dia é: ",media,"+-",desv)
+for k in range(0,len(dados)):
+    soma=soma+dados[k]
+    cont = cont+1
+    if(k>=6):
+        mediamovel.append(soma/cont)
+
+plt.plot(mediamovel)
+plt.show()
