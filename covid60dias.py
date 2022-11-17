f8f2aa9bc539853d53329408c1708d27a9f0170b
Manteiga <rafael.manteiga@hotmail.com>
2022-11-17 03:56:54 -0300

    Add files via upload

---
+++ covid60dias.py
@@ -0,0 +1,54 @@
+import statistics as lr
+import numpy as np
+import matplotlib.pyplot as plt
+
+dados = []
+mediamovel = []
+x = []
+cont=0
+soma=0
+
+# Rodando o arquivo para incluir os dados no vetor
+with open("C:\\Users\\f0fp1107\Desktop\\covid60dias.txt") as f:
+    for line in f:
+        dados.append(float(line))
+
+# Calculando a média e o desvio padrão com 2 casas decimais
+media = np.average(dados).round(2)
+desv = np.std(dados).round(2)
+
+# Salvando a média num arquivo
+arq = open("media.txt","a")
+arq.write('A média de casos por dia é: ')
+arq.write(str(media))
+arq.write("+-")
+arq.write(str(desv))
+arq.close()
+
+# Calculando as médias móveis e incluindo no vetor
+for k in range(0,len(dados)):
+    soma=soma+dados[k]
+    cont = cont+1
+    if(k>=int(np.sqrt(len(dados)))):
+        mediamovel.append(soma/cont)
+
+# Limitando os valores de x inferiormente para começar a partir de onde a média móvel passou a ser calculada
+for k in range(int(np.sqrt(len(dados))),len(dados)):
+    x.append(k)
+
+# Achando coeficientes da regressão linear
+a1,a2=lr.linear_regression(x,mediamovel)
+
+# Limitando o x da reta de regressão
+xi = np.linspace(int(np.sqrt(len(dados))),120)
+
+# Plotando a reta usando os coeficientes
+plt.plot(xi,a1*xi+a2)
+
+# Plotando o gráfico da média móvel
+plt.plot(x,mediamovel)
+
+# Salvando uma imagem do gráfico gerado
+plt.savefig("grafico.png")
+
+# Caso seja necessário mostrar o gráfico, é só usar plt.show()
